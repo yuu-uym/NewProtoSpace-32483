af4fed01808f4d478ad6f470b8c6dfe59711e504
yuu-uym <uyama0816y@gmail.com>
2020-12-20 18:45:01 +0900

    名前の変更

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Protospace32483
+module NewProtoSpace32483
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
